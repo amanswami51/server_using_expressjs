10bbf0f7af45e4652e8abd137ec0d97e1be45342
Aman <123ramramji456@gmail.com>
2024-02-12 21:55:07 +0530

    initial-commit

--- app.js
+++ app.js
@@ -1,61 +1,26 @@
-//*****************notes*****************
-//1. npm init -y 
-//2. npm install express
-//3. npm i mongoose@6.6.5 
-//4. npm i --save-dev nodemon
-//5. import express, make app, then listen server
-//6. import mongoose, mongoose.connect, make schema, make model, then app.post 
-//7. npm install pug
-//Template Inheritance
-//pug supports template inheritance. Template inheritence works via the "block" and "extends" keywords.
-//"base.pug" file is an example of Template Inheritance
-//8. make views folder //make all pug html files in this folder.
-//9. make static folder  //make all html, css, javascript files in this folder. These files are imported by pub html files.
-//10. npm install body-parser , this is optional
-
-
-
-
-
-
-// ******************tut_72******************(Just Example of express and pug)
-// const express = require("express") //Import express module
-// const path = require("path");
-// const app = express();  //make express app
-// const port = 80;
-
-// app.get("/", (req, res) => {
-//     res.send("This is my first express app with harry youtube chennel")
-// });
-// app.get("/about", (req, res) => {
-//     res.status(200).send("This is my about page first express app with harry")  //also we send status code like this
-// });
-
-// // app.post("/contect", (req, res)=>{
-// //     res.send("This is my first express app with harry")
-// // });
-
-
-// //for serving static files, static files are those files which are publically available and everyone can download without any restriction 
-// app.use('/static', express.static('static'));  // // " localhost/static/first.js " //write this in browser window to access these  
-
-// //*********pug*********"pugjs.org" read more here
-// //nodejs express template engine, example:- pug     //npm install pug
-// app.set('view engine', 'pug') //set the template engine as pug
-// app.set('views', path.join(__dirname, 'views')) //set the views directory
-// app.get('/demo', (req, res) => {    //our pug demo endpoint
-//     res.status(200).render('demo', { title: 'Hey', message: 'Hello there!' })
-// });
-// app.get('/second', (req, res)=>{     //our pug second endpoint
-//     const con = "This is the best content on the internet."
-//     const params = {'title':'PubG is best game', 'content':con} 
-//     res.status(200).render('SecondPug', params);
-// });
-
-// // ***************Execute server***************
-// app.listen(port, () => {
-//     console.log(`The application started successfully on port ${port}`)
-// })
+//*****************server by using express notes*****************
+//1. npm init -y
+//2. create "app.js" file.
+//      --npm install express
+//      --Import express through "const express = require("express")" .
+//      --create server through "const app = express()" method. 
+//      --then run server through "app.listen(port,hostname, callback)" method.
+//3. use "express.static('static')" mathod for serving static folder.
+//4. use "express.urlencoded()" method for access form data by using "req.body".
+//5. set "pug" for serving files through "res.render('pug file name')" method. 
+//      --npm install pug
+//      --setup through " app.set('view engine', 'pug'); app.set('views', path.join(__dirname, 'views'));".
+//      --pug is a nodejs express template engine. 
+//      --pug supports template inheritance. Template inheritence works via the "block" and "extends" keywords.
+//      --"base.pug" file is an example of Template Inheritance
+//6. create routes (main logic) like "app.get(path, callback)", "app.post(path, callback)" etc.
+//7. Mongodb database setup
+//      --install mongoose through "npm i mongoose@6.6.5" command.
+//      --import mongoose through "const mongoose = require('mongoose')".
+//      --connect to db through "mongoose.connect(URI)" method.
+//      --make schema(formate) through "mongoose.Schema(inObjectFormat)" method.
+//      --create model through "mongoose.model("collection-name", schema)" method.
+//      --Add data in database through "modelName(dataObject)" method. then use "save()" method.
 
 
 
@@ -64,25 +29,36 @@
 
 
 
-// ******************tut_73******************(Actually implementation of the express and pug) //data store in contact.txt file
-// const express = require("express") //Import express module
+
+
+
+
+
+// ******************data store in contact.txt file******************
+// const express = require("express") //Import express
 // const path = require("path");
 // const fs = require("fs");
-// const app = express();  //make express app
+
+// const hostname = 'localhost';
 // const port = 80;
+// const app = express();  //create server //like "http.createServer"
  
- 
-// //EXPRESS SPECIFIC STUFF
-// app.use('/static', express.static('static')); //for serving static files
+
+// //for serving static files, static files are those files which are publically available and everyone can download without any restriction 
+// app.use('/freefile', express.static('static'));  // // " localhost/static/first.js " //write this in browser window to access these  
 // app.use(express.urlencoded());  //use to access form data
 
 
-// //PUG SPECIFIC STUFF
+// //pug setup**********************
 // app.set('view engine', 'pug') //set the template engine as pug
 // app.set('views', path.join(__dirname, 'views')) //set the views directory
 
 
-// //ENDPOINTS
+// //EndPoints OR Routes OR CheckPoints
+// app.get("/first", (req, res) => {
+//     res.send("This is my first express app with harry youtube chennel")
+// });
+
 // app.get('/', (req, res)=>{
 //     res.status(200).render('01_index')
 // });
@@ -115,9 +91,9 @@
 //     res.status(200).render('03_payment_form')
 // });
 
-// // Execute server
-// app.listen(port, () => {
-//     console.log(`The application started successfully on port ${port}`)
+// // ***************listen server***************
+// app.listen(port,hostname, () => {
+//     console.log(`The application started successfully http://${hostname}:${port}`);
 // })
 
 
@@ -125,21 +101,24 @@
 
 
 
-//**********************tut_88**********************(Actually implementation of the express and pug) //data store in database name mongodb
-const express = require('express'); //Import express module
-const app = express();  //make express app
-const port = 5000;
-const path = require("path"); //use to set the views directory
+//**********************data store in database name mongodb**********************
+const express = require('express'); //Import express
+const path = require("path");
 
-// *********************connect mongoose*********************
+const hostname = 'localhost';
+const port = 5000;
+const app = express();  //create server
+
+//Mongodb setup*********************
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/crudoperation",(err)=>{
-    if(err) console.log(`Unable to connect to the server : ${err}`);
-    else 
-        console.log("mongodb is connected");
+
+mongoose.connect("mongodb://127.0.0.1:27017/crudoperation").then(()=>{
+    console.log("mongodb is connected");
+}).catch((err)=>{
+    console.log(`Unable to connect to the server : ${err}`);
 })
 
-// schema
+//create schema************************
 const UserSchema = new mongoose.Schema({
     fname: {
         type: String,
@@ -166,21 +145,21 @@ const UserSchema = new mongoose.Schema({
         required: [true, "DOB is required"]
     }
 })
-// model
+//create model****************************************
 const UserModel = mongoose.model("user", UserSchema) //plural of "user" is collection name
 
 
-//***************EXPRESS SPECIFIC STUFF***************
+//serving static files****************************
 app.use('/static', express.static('static')); //for serving static files
 app.use(express.urlencoded({extended: true}));  //use to access form data (req.body)
 
 
-// ***************PUG SPECIFIC STUFF***************
-app.set('view engine', 'pug') //set the template engine as pug
-app.set('views', path.join(__dirname, 'views')) //set the views directory
+//pug setup**********************
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
 
-// ***************ENDPOINTS***************
+//ENDPOINTS (Routes)***************
 app.get('/', (req, res)=>{
     res.status(200).render('01_index')
 });
@@ -203,7 +182,7 @@ app.post("/contect", (req, res)=>{
     });
 })
 
-// ***************Execute server***************
-app.listen(port, () => {
-    console.log(`The application started successfully on port ${port}`)
+//listen server*************************
+app.listen(port,hostname, () => {
+    console.log(`The application started successfully http://${hostname}:${port}`);
 })
